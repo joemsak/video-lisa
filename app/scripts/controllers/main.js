ee47218a8886c00a6b983f6f5bd90385894a0a6d
Joe Sak <joe@joesak.com>
2015-02-07 22:51:32 -0500

    Initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name videolisaApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the videolisaApp
+ */
+angular.module('videolisaApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
